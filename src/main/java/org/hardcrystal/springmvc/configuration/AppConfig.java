ca43b7fd245d0e748a7ce7cf42a552053b286f69
Sergey Karpov <karpov.srg@gmail.com>
2016-01-22 00:25:33 +0200

    Implemented: Resource handler (for static objects. Added: Spring Initializer for app.

--- src/main/java/org/hardcrystal/springmvc/configuration/AppConfig.java
+++ src/main/java/org/hardcrystal/springmvc/configuration/AppConfig.java
@@ -2,11 +2,13 @@ package org.hardcrystal.springmvc.configuration;
 
 import org.hardcrystal.springmvc.converter.RoleToUserProfileConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -34,5 +36,34 @@ public class AppConfig extends WebMvcConfigurerAdapter{
         registry.viewResolver(viewResolver);
     }
 
+    /**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
 
+    /**
+     * Configure Converter.
+     * A converter to convert string values[Roles] to UserProfiles in newUser.jsp
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(roleToUserProfileConverter);
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer matcher) {
+        matcher.setUseRegisteredSuffixPatternMatch(true);
+    }
 }
---
+++ src/main/java/org/hardcrystal/springmvc/configuration/AppInitializer.java
@@ -0,0 +1,24 @@
+package org.hardcrystal.springmvc.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by Smile on 22.01.2016.
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
