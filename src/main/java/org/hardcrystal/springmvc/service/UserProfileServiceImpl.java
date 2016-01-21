ed174644e8c8fc9617d32d7b0ab1292f0de08bc6
Kaidzen <karpov.srg@gmail.com>
2016-01-21 19:43:52 +0200

    Implementetion of Service layer: interfaces. Added classes with implementations.

---
+++ src/main/java/org/hardcrystal/springmvc/service/UserProfileServiceImpl.java
@@ -0,0 +1,32 @@
+package org.hardcrystal.springmvc.service;
+
+import org.hardcrystal.springmvc.dao.UserProfileDao;
+import org.hardcrystal.springmvc.model.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 21.01.2016.
+ */
+@Service("userPofileService")
+@Transactional
+public class UserProfileServiceImpl implements UserProfileService {
+
+    @Autowired
+    UserProfileDao dao;
+
+    public UserProfile findById(int id) {
+        return dao.findById(id);
+    }
+
+    public UserProfile findByType(String type) {
+        return dao.findByType(type);
+    }
+
+    public List<UserProfile> findAll() {
+        return dao.findAll();
+    }
+}
---
+++ src/main/java/org/hardcrystal/springmvc/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package org.hardcrystal.springmvc.service;
+
+import org.hardcrystal.springmvc.dao.UserDao;
+import org.hardcrystal.springmvc.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 21.01.2016.
+ */
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao dao;
+
+    public User findById(int id) {
+        return dao.findById(id);
+    }
+
+    public User findBySSO(String sso) {
+        return dao.findBySSO(sso);
+    }
+
+    public void saveUser(User user) {
+        dao.save(user);
+    }
+
+    public void updateUser(User user) {
+        User entity = dao.findById(user.getId());
+        if (entity != null){
+            entity.setSsoId(user.getSsoId());
+            entity.setEmail(user.getEmail());
+            entity.setFirstName(user.getFirstName());
+            entity.setLastName(user.getLastName());
+            entity.setPassword(user.getPassword());
+            entity.setUserProfiles(user.getUserProfiles());
+        }
+    }
+
+    public void deleteUserBySSO(String sso) {
+        dao.deleteBySSO(sso);
+    }
+
+    public List<User> findAllUsers() {
+        return dao.findAllUsers();
+    }
+
+    public boolean isUserSSOUnique(Integer id, String sso) {
+        User user = findBySSO(sso);
+        return ( user == null || ((id != null) && (user.getId() == id)));
+    }
+}
